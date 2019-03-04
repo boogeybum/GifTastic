39c4f37f14639e1ff37017130fb8ebf5d9145824
boogeybum <45719473+boogeybum@users.noreply.github.com>
2019-03-03 22:18:21 -0600

    Added button on click function to request giphyAPI data and return data to page. Currently not returning data.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 // declare variables
-var topics = ["Tommy Boy", "Holy Grail", "Caddy Shack", "Happy Gilmore"];
+var topics = ["Tommy Boy", "Holy Grail", "Caddyshack", "Happy Gilmore"];
 
 // update buttons on page from topics array
 function renderButtons() {
@@ -13,8 +13,43 @@ function renderButtons() {
         newBtn.attr("data-movie", topics[i]);
         newBtn.text(topics[i]);
         $("#btn-view").append(newBtn);
+        // console.log(newBtn);
     }
 };
+
+// search for gif's based on button clicked
+$("button").on("click", function() {
+    var movieGif = $(this).attr("data-movie");
+    console.log(movieGif);
+    // link to giphy api
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    movieGif + "&api_key=TD2V1oeol0vQTK3C0op1DCFEbEknqsRW&limit=5";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++) {
+            if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                var gifDiv = $("<div>");
+
+                var rating = results[i].rating;
+                var p = $("<p>").text("Rating: " + rating);
+                var gifImage = $("<img>");
+                gifImage.attr("src", results[i].images.fixed_height.url);
+                gifDiv.append(gifImage);
+                gifDiv.append(p);
+                $("#giftastic").prepend(gifDiv);
+            }
+        }
+    });
+});
+
+// request data from giphy based on the button clicked and return random gif's that meet the requirements.
+
+// prepend the returned gif's to add them to the page just below the buttons
 
 // add buttons to topics array based on user input (need if else statement to prevent adding duplicate entries and empty buttons)
 $("#add-movie").on("click", function(event) {
@@ -27,10 +62,4 @@ $("#add-movie").on("click", function(event) {
     $("#gifSearch-input").val("");
 });
 
-
-
-// request data from giphy based on the button clicked and return random gif's that meet the requirements.
-
-// prepend the returned gif's to add them to the page just below the buttons
-
-renderButtons();
+// renderButtons();
